17e94795d403c901abc837f096cc62eee154d837
AbdallahEhab <abdallahehab3710@gmail.com>
2026-02-06 23:27:13 +0200

    feature: added player and background sprites

--- src/rendering/gameworld/GamePanel.java
+++ src/rendering/gameworld/GamePanel.java
@@ -8,18 +8,29 @@ import entities.Entity;
 import states.MoveState;
 import vector.Position;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class GamePanel extends JPanel {
     private Actor player;
+    private BufferedImage playerImage;
+    private BufferedImage backgroundImage;
     boolean[] keys = new boolean[5];
+    private final Movable walkMovement = new WalkMovement();
+    private final Movable runMovement  = new RunMovement();
+
     public GamePanel(){
         this.setBackground(Color.white);
         initPlayer();
+        loadBackgroundImage();
+        loadPlayerImage();
         setFocusable(true);
         addKeyListener(new KeyAdapter(){
             @Override
@@ -51,10 +62,10 @@ public class GamePanel extends JPanel {
 
     }
     void handleInput(){
-        if(keys[4]){
-            player.setMovable(new RunMovement());
-        }else{
-            player.setMovable(new WalkMovement());
+        if (keys[4]) {
+            player.setMovable(runMovement);
+        } else {
+            player.setMovable(walkMovement);
         }
         float speed = this.player.getMovable().getSpeed();
         float dx = 0;
@@ -69,6 +80,20 @@ public class GamePanel extends JPanel {
         this.handleInput();
         this.player.update();
     }
+    void loadPlayerImage(){
+        try{
+            playerImage = ImageIO.read(getClass().getResource("/res/sprites/mogging_barbarian.png"));
+        }catch(IOException e){
+            System.err.println("Failed to load player.png");
+        }
+    }
+    void loadBackgroundImage(){
+        try{
+            backgroundImage = ImageIO.read(getClass().getResource("/res/sprites/grass_tile.png"));
+        }catch(IOException e){
+            System.err.println("Failed to load background.png");
+        }
+    }
     void initPlayer(){
         Movable movable = new WalkMovement();
         this.player = new Actor(new Entity("zelda",100,new Position(10f,10f),true), null,movable,null);
@@ -78,7 +103,19 @@ public class GamePanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        if(backgroundImage != null){
+            for(int x = 0; x < getWidth(); x+=500){
+                for(int y = 0; y < getHeight(); y+= 500){
+                    g.drawImage(backgroundImage, x, y,500,500, this);
+                }
+            }
+        }
+        if(playerImage != null){
+
+            g.drawImage(playerImage, (int)player.getEntity().getPosition().getX(), (int)player.getEntity().getPosition().getY(),200,300, this);
+        }else{
         g.setColor(Color.red);
         g.fillRect((int)player.getEntity().getPosition().getX(),(int)player.getEntity().getPosition().getY(),100,100);
+        }
     }
 }
